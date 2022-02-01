555bce806cecea9e84073896aa114345e16ddede
b8018716 <b8018716@shu.ac.uk>
2022-02-01 14:30:01 +0000

    Added empty html pages and added product model

--- reviewApp/product/admin.py
+++ reviewApp/product/admin.py
@@ -1,3 +1,10 @@
+from itertools import product
 from django.contrib import admin
+from .models import Product
+
+@admin.register(Product)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display=['name','brand']
 
 # Register your models here.
--- reviewApp/product/models.py
+++ reviewApp/product/models.py
@@ -1,3 +1,18 @@
+from unicodedata import category
 from django.db import models
+
+
+class Product(models.Model):
+    productid = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    brand = models.CharField(max_length=100)
+    averagecost = models.DecimalField(decimal_places=2, max_digits=8)
+    category = models.CharField(max_length=100)
+    datereleased = models.DateField()
+    description = models.TextField(blank=True)
+    productphoto = models.ImageField()
+
+def __str__(self):
+    return self.name
 
 # Create your models here.
