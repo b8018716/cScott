b38b168336a8fb0a36e003b5fd3ef1e6bd07b75a
b8018716 <63415164+b8018716@users.noreply.github.com>
2022-03-31 15:01:19 +0100

    [All minumum requirements complete] [All basic requirements complete]
    Changed model to have child models of Product
    
    Implemented product details page with review for product displayed

--- reviewApp/product/admin.py
+++ reviewApp/product/admin.py
@@ -1,8 +1,9 @@
 from atexit import register
+from csv import list_dialects
 from itertools import product
 from pyexpat import model
 from django.contrib import admin
-from .models import Product, Review
+from .models import Product, Review, Phone, Tablet, Laptop
 
 class reviewInLine(admin.TabularInline):
     model = Review
@@ -16,13 +17,27 @@ class ProductAdmin(admin.ModelAdmin):
     list_display=['name','brand',]
     inlines = [reviewInLine]
 
+    
+
 
 class ReviewAdmin(admin.ModelAdmin):
    list_display=['author','product','productRating','datePosted',]
+        
    
+class PhoneAdmin(ProductAdmin):
+    list_display=['name','brand','averagecost','dimensions','datereleased']
+
+class LaptopAdmin(ProductAdmin):
+    list_display=['name','brand','averagecost','operatingsystem','amountOfRam','memorySize','processor','datereleased']
+
+class TabletAdmin(ProductAdmin):
+    list_display=['name','brand','averagecost','screensize','datereleased']
 
 
 
 
+admin.site.register(Laptop, LaptopAdmin)
+admin.site.register(Tablet, TabletAdmin)
+admin.site.register(Phone, PhoneAdmin)
 admin.site.register(Review, ReviewAdmin)
 admin.site.register(Product, ProductAdmin)
